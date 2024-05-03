1abbe76d3703498ed32b4ed75a699bff17458099
anthonyadcs <anthonydacsilva10@gmail.com>
2024-05-03 16:36:59 -0300

    adição e estilização dos componentes de acordo com o desafio no arquivo 'README'

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
-import './Header.css'
-
+import {Box, Text} from '@chakra-ui/react'
+import {DIO} from './img/DIO'
 export const Header  = () => {
   return(
-    <div className='header'>
-      Dio Bank
-    </div>
+    <Box display="flex" alignItems="flex-end" gap="10px" paddingLeft="20px" padding="20px">
+      <DIO/>
+      <Text fontWeight="extrabold" fontSize="2xl"alignSelf="end" bgGradient='linear(to-r, #E94D5F, #54539C, #31A2DB)' bgClip='text'>BANK</Text>
+    </Box>
   )
 }
